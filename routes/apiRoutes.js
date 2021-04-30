4fdfb01a393fc30cbbc892fd9c0dbb1bcbc00701
Jacob Studer <jacob.studer.dev@gmail.com>
2021-04-29 22:58:04 -0600

    recent changes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,8 +1,10 @@
 const notesData = require('../db/db.json');
 
 module.exports = (app) => {
-    app.get('/api/db', (req, res) => res.json(notesData));
+    app.get('/api/notes', (req, res) => res.json(notesData));
 
+
+    
     app.post('/api/db', (req, res) => {
         const newNote = req.body;
         console.log(newNote);
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 
 // ROUTER
 // The below points our server to a series of "route" files.
