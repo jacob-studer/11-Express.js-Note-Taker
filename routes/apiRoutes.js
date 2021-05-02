cd1b0a3a4d7056bc6ce3d5b6c9929ea6ff0e5262
Jacob Studer <jacob.studer.dev@gmail.com>
2021-05-02 11:19:21 -0600

    working app, few last changes needed

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,14 +1,49 @@
-const notesData = require('../db/db.json');
+const router = require('express').Router();
+const fs = require('fs');
+const path = require('path');
+const { v4: uuidv4 } = require('uuid');
+// const allnotes = require('../db/db.json');
 
-module.exports = (app) => {
-    app.get('/api/notes', (req, res) => res.json(notesData));
 
+
+    router.get('/api/notes', (req, res) => {
+        fs.readFile(path.join('./db/db.json'), 'utf8', (err, notesData) => {
+            if (err) throw err;
+            console.log(notesData);
+            res.json(JSON.parse(notesData))
+        })
+        });
+
+    //2nd method using global allnotes variable
+
+    // router.get('/api/notes', (req, res) => {
+    //     res.send(allnotes);
+    // });
+
+
+
+    router.post('/api/notes', (req, res) => {
+        const notes = JSON.parse(fs.readFileSync(path.join('./db/db.json')));
+        const newNote = req.body;
+        newNote.id = uuidv4();
+        notes.push(newNote);
+        fs.writeFileSync(path.join('./db/db.json'), JSON.stringify(notes))
+        console.log();
+        });
 
     
-    app.post('/api/db', (req, res) => {
-        const newNote = req.body;
-        console.log(newNote);
-        
+    //2nd method using global allnotes variable
+    
+    // router.post('/api/notes', (req, res) => {
+    //     const newNote = req.body;
+    //     newNote.id = uuidv4();
+    //     allnotes.push(newNote);
+    //     fs.writeFileSync(path.join('./db/db.json'), JSON.stringify(allnotes))
+    //     res.status(200);
+    // });
 
-        });
-};
+    //for delete, loop through notes array and find the matching id to the note being deleted then make a router.delete
+
+
+
+    module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,14 @@
+const router = require('express').Router();
 const path = require('path');
 
 
-module.exports = (app) => {
-
-app.get('/notes', (req, res) => {
+router.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/notes.html'));
     });
 
-app.get('*', (req, res) => {
+router.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
     });
-};
+
+
+module.exports = router;
